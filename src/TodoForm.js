3b5a0586877f6361083c0d78b9164bd23c3af97a
Imre Bartis <imre.bartis@siili.com>
2019-12-05 17:26:18 +0200

    split into 2 contexts

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import { Paper, TextField } from "@material-ui/core";
 import useInputState from "./hooks/useInputState";
-import { TodosContext } from "./context/todos.context";
+import { DispatchContext } from "./context/todos.context";
 
 const TodoForm = () => {
   const [value, handleChange, reset] = useInputState("");
-  const { dispatch } = useContext(TodosContext);
+  const dispatch = useContext(DispatchContext);
   return (
     <Paper style={{ margin: "10px 0", padding: "0 10px" }}>
       <form
