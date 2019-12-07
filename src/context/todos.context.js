a82f5cbd4752a73c45242ed7244246649a04169b
Imre Bartis <imre.bartis@siili.com>
2019-12-07 04:41:39 +0200

    custom hook: reducer + local storage

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,5 +1,6 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
 import todoReducer from "../reducers/todo.reducer.js";
+import { useLocalStorageReducer } from "../hooks/useLocalStorageReducer.js";
 
 const defaultTodos = [
   { id: 1, task: "Mow the lawn using goats", completed: false },
@@ -10,11 +11,17 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
 
   return (
     <TodosContext.Provider value={todos}>
-      <DispatchContext.Provider value={dispatch}>{props.children}</DispatchContext.Provider>
+      <DispatchContext.Provider value={dispatch}>
+        {props.children}
+      </DispatchContext.Provider>
     </TodosContext.Provider>
   );
 }
